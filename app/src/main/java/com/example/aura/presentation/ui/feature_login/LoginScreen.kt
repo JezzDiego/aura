b5d2d21090a693d89a7030ca2c8bebaece9ab97f
Felipe-MML <felipemaras20@gmail.com>
2025-11-07 16:24:37 -0300

    Feat: Adição da tela de Login

---
+++ app/src/main/java/com/example/aura/presentation/ui/feature_login/LoginScreen.kt
@@ -0,0 +1,155 @@
+package com.example.aura.presentation.ui.feature_login
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Key
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier
+){
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+        ) {
+            Text(
+                text = "AURA",
+                style = MaterialTheme.typography.titleLarge,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            Box(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.secondaryContainer, RoundedCornerShape(16.dp))
+                    .padding(horizontal = 40.dp, vertical = 16.dp)
+            ) {
+                Text(
+                    text = "Digite suas credenciais para Entrar",
+                    fontSize = 15.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            EmailInput()
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            PasswordInput()
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            Button(
+                onClick = {},
+                modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                shape = RoundedCornerShape(999.dp)
+            ) {
+                Text("Entrar", fontSize = 18.sp)
+            }
+
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EmailInput(){
+    var email by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = email,
+        onValueChange = {email = it},
+        label = {Text("E-mail")},
+        leadingIcon = { Icon(Icons.Default.Email, contentDescription = null) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(999.dp)
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordInput(){
+    var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = password,
+        onValueChange = {password = it},
+        label = {Text("Senha")},
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth(),
+        visualTransformation = if(passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        trailingIcon = {
+            val image = if (passwordVisible)
+                Icons.Filled.Visibility
+            else
+                Icons.Filled.VisibilityOff
+
+            IconButton(onClick = {passwordVisible = !passwordVisible}) {
+                Icon(imageVector = image, contentDescription = null)
+            }
+        },
+        leadingIcon = { Icon(Icons.Default.Lock, contentDescription = null) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        shape = RoundedCornerShape(999.dp)
+    )
+}
+
+
+@Preview
+@Composable
+fun PreviewLoginScreen(){
+    LoginScreen()
+}
