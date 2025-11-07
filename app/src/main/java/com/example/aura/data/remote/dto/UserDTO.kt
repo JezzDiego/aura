89bdd50f062ea44d66205005cabbb7d30f7469b4
Felipe-MML <felipemaras20@gmail.com>
2025-11-07 18:02:32 -0300

    Feat: Adição do userAPI e do UserDTO

--- app/src/main/java/com/example/aura/data/remote/dto/UserDTO.kt
+++ app/src/main/java/com/example/aura/data/remote/dto/UserDTO.kt
@@ -7,6 +7,7 @@ data class UserDTO(
     @SerializedName("id") val id: String,
     @SerializedName("name") val name: String,
     @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
     @SerializedName("birthDate") val birthDate: String?,
     @SerializedName("gender") val gender: String?,
     @SerializedName("plan") val plan: SubscriptionPlan
