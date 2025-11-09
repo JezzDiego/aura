28062a10ff4aaba9cb6f0ccc8fc9c27fdf4bcc3e
Felipe-MML <felipemaras20@gmail.com>
2025-11-09 08:33:59 -0300

    Feat: Criando a entidade de cliente

---
+++ app/src/main/java/com/example/aura/data/local/entity/UserEntity.kt
@@ -0,0 +1,12 @@
+package com.example.aura.data.local.entity
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class UserEntity (
+    @PrimaryKey val id: String,
+    val name: String,
+    val email: String,
+    val birthDate: String,
+    val gender: String
+)
